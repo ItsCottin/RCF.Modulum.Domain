7cf0a1389a1dbef5a0ca104bd79e1b78c75feb2b
Rodrigo Cotting <cottingfontes@hotmail.com>
2025-04-15 23:21:23 -0300

    Adicionado model NugetPacote para controle de versao em base

---
+++ Entities/NugetPacote.cs
@@ -0,0 +1,16 @@
+﻿using modulum.Domain.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modulum.Domain.Entities
+{
+    public class NugetPacote : AuditableEntity<int>
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Versao { get; set; }
+    }
+}
